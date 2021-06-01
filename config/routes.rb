9748701a8facd4474427261a5ec023011c6f2375
radin reth <radin.reth@gmail.com>
2021-06-01 16:33:31 +0700

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'abouts#show'
+
+  get '/faq', to: 'faq#index'
+  resource :about, only: [:show]
 end
