ce9435dffa85c9c2bab6199473455576bc628a42
radin reth <radin.reth@gmail.com>
2021-10-13 09:41:18 +0700

    Add google analytic

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Get tracking_id from Google analytic website
+  # https://analytics.google.com/
+  GA.tracker = ENV['GA_TRACKING_ID']
 end
