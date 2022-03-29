654f4443fb09b3fdeea1a45edb3a66a52416d1c4
Sokly <hengsokly23@gmail.com>
2022-03-29 16:54:49 +0700

    Website: add ui v2

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,9 @@ module ScorecardWebsite
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.i18n.fallbacks = [:en]
+    config.i18n.available_locales = [:en, :km]
+    config.i18n.default_locale = :km
   end
 end
